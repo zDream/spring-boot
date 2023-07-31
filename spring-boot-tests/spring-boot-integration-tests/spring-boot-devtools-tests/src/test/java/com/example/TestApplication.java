c59b3d992c9f9602b8527fb7c78b4f9bb29d99d7
zhutongtong <zhutongtong@xinzhirun.cn>
2023-07-31 11:39:10 +0800

    添加demo

---
+++ spring-boot-tests/spring-boot-integration-tests/spring-boot-devtools-tests/src/test/java/com/example/TestApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhutongtong
+ * @date 2023/7/31 11:34
+ */
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
